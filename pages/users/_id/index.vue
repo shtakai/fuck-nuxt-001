c73ff6b0b4cfba727f4015e906a215c09d0e3f4e
Alyson Takai <shtakai@gmail.com>
2019-08-04 12:47:01 +0700

    Add dynamic route for users

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>single user page suck em</h2>
+    <h3>{{ $route.params.id }}</h3>
+    <h3>{{ $route.params }}</h3>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>This is users page fuck off</h2>
+</template>
+
+<script>
+export default {}
+</script>
